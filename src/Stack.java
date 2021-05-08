1c62aaf2d13eedb11c6ccd296a7edc6030d936d5
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-08 23:21:54 +0530

    Partially build up pushing method

---
+++ src/Stack.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Stack {
+    static int stackArray[] = {1, 2, 3, 4, 5};
+
+    public static void main(String[] args) {
+
+        Stack stack = new Stack();
+        stack.push(7);
+    }
+
+
+    public void push(int num) {
+        int stackArrayPushed[] = new int[stackArray.length+1];
+        for (int i = 0; i < stackArrayPushed.length-1; i++) {
+            if (i < stackArrayPushed.length) {
+                stackArrayPushed[i] = stackArray[i];
+            } else {
+                stackArrayPushed[i] = num;
+            }
+        }
+        System.out.println(Arrays.toString(stackArrayPushed));
+
+    }
+
+    public void pop() {
+
+    }
+
+    public boolean empty() {
+        return true;
+    }
+
+    public void clear() {
+
+    }
+
+    public void print() {
+
+    }
+
+    public boolean constains(int num) {
+        return true;
+    }
+
+
+}
