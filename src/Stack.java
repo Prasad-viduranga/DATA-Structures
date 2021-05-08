21d329f888ab76cae08ae4b3a869b27ab5f2be53
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-09 00:54:04 +0530

    build up the empty method

--- src/Stack.java
+++ src/Stack.java
@@ -7,15 +7,18 @@ public class Stack {
 
         Stack stack = new Stack();
         stack.push(7);
-        stack.pop();
 
+        System.out.println(stack.empty());
         stack.print();
+        stack.pop();
+        stack.clear();
+        stack.print();
+        System.out.println(stack.empty());
     }
 
 
     public void push(int num) {
         int stackArrayPushed[] = new int[stackArray.length + 1];
-
         for (int i = 0; i < stackArrayPushed.length; i++) {
 
             if (i < stackArray.length) {
@@ -24,31 +27,29 @@ public class Stack {
                 stackArrayPushed[i] = num;
             }
         }
-        //System.out.println(Arrays.toString(stackArrayPushed));
         stackArray = stackArrayPushed;
     }
 
     public void pop() {
-/*        int stackArrayPushed[] = new int[stackArray.length+1];
-
-        for (int i = 0; i < stackArrayPushed.length; i++) {
-
-            if (i < stackArray.length) {
-                stackArrayPushed[i] = stackArray[i];
-            } else {
-                stackArrayPushed[i] = num;
-            }
+        int stackArrayPoped[] = new int[stackArray.length - 1];
+        for (int i = 0; i < stackArrayPoped.length; i++) {
+            stackArrayPoped[i] = stackArray[i];
         }
-        System.out.println(Arrays.toString(stackArrayPushed));*/
+        stackArray = stackArrayPoped;
+
 
     }
 
     public boolean empty() {
-        //if (stackArray)
-        return true;
+        if (stackArray.length > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public void clear() {
+
 
     }
 
