c6b2f0d852c9fcf3d0090453841cb380a3331fe0
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-08 22:56:28 +0530

    Initialize a class and methods

---
+++ src/stack.java
@@ -0,0 +1,35 @@
+public class stack {
+
+    public static void main(String[] args) {
+
+        int size = 0;
+        int stackArray[] = {};
+
+
+    }
+
+    public void push() {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public boolean empty() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void print() {
+
+    }
+
+    public boolean constains(int num) {
+
+    }
+
+}
