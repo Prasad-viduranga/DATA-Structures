9b069622163c3baf1c5e8d2bff12d769bbaa4da9
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-09 08:51:40 +0530

    Impliment methods

--- Queue/src/ds/Queue.java
+++ Queue/src/ds/Queue.java
@@ -1,4 +1,46 @@
 package ds;
 
 public class Queue {
+
+    public void enqueue() {
+
+
+    }
+
+    public void dequeue() {
+
+
+    }
+
+    public void print() {
+
+
+    }
+
+    public void clear() {
+
+
+    }
+
+    public int size() {
+
+
+    }
+
+    public int peek() {
+
+
+    }
+
+    public boolean contains() {
+
+        return false;
+    }
+
+    public boolean empty() {
+
+        return true;
+    }
+
+
 }
